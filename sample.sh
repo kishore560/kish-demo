f32c263bb547389e48b6a3253c651fda04b87d0d
kishore123155 <you@example.com>
2020-11-30 22:45:26 +0530

    adding very first line

---
+++ sample.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# echo and printf are two commands can help in printing.
+# we can use echo command because it's more user friendly, yet supports all the requirements.
+
+echo hello world
