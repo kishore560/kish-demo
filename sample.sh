c564cee36d0a18de7d3f7e3eee8c75c5dbb923a0
kishore123155 <you@example.com>
2020-11-30 22:52:58 +0530

    adding very first line

--- sample.sh
+++ sample.sh
@@ -4,3 +4,17 @@
 # we can use echo command because it's more user friendly, yet supports all the requirements.
 
 echo hello world
+
+# it prints hello world on the screen.
+
+# print two lines
+echo line1
+echo line2
+#print two lines with single echo command
+#this can be done with the help of escape sequences
+
+#\n - new line
+#\t - new tab
+#\e - new colour
+
+echo -e "line1\nline22"
